3113054578f047ddec6431f41e38c48a2be0a364
u18177657 <arkipovkirill43@gmail.com>
2019-12-19 18:12:14 +0300

    тут типа самое начал проекта

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+} from "react-router-dom";
+
+import {Header} from '../header/header';
+import './App.scss';
 
 const App: React.FC = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+        <Route>
+            <Header/>
+        </Route>
+
+    </Router>
   );
+
 }
 
-export default App;
+export {App};
---
+++ src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import './header.scss';
+
+const Header: React.FC = () => {
+    return (
+        <div className="App">
+            <p>привет</p>
+        </div>
+    );
+}
+
+export {Header};
